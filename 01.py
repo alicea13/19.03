62e694db24c6cb54f9899f4d69dae83d860df992
alisa <alica.aisina@yandex.ru>
2020-03-19 14:49:43 +0300

    ok

---
+++ 01.py
@@ -0,0 +1,19 @@
+from flask import Flask
+# from flask_ngrok import run_with_ngrokith_ngrok
+
+app = Flask(__name__)
+# run_with_ngrok(app)
+
+
+@app.route("/")
+def index():
+    return "Привет от Алисы"
+
+
+@app.route("/about")
+def about():
+    return "Text"
+
+
+if __name__ == '__main__':
+    app.run()
